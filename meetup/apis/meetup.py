db6a4792a05a0efd3265b71f3c092c240a4d3b47
lonnie <joonyeol.dev@gmail.com>
2024-12-27 17:08:36 +0900

    [PH-4] 모임 수정 폼데이터 받도록 변경

--- meetup/apis/meetup.py
+++ meetup/apis/meetup.py
@@ -1,18 +1,19 @@
-from ninja import Router, File, UploadedFile
+# -*- coding: utf-8 -*-
+from django.db import transaction
 from django.shortcuts import get_object_or_404
+from ninja import File, Router, UploadedFile
+
 from meetup.models import Meetup, Member
 from meetup.schemas.meetup import (
     MeetupCreateSchema,
-    MeetupResponseSchema,
     MeetupListResponseSchema,
+    MeetupResponseSchema,
     OrganizerSchema,
 )
+from placeholder.schemas.base import ResultSchema
+from placeholder.utils.auth import JWTAuth
 from placeholder.utils.decorators import handle_exceptions
 from placeholder.utils.exceptions import UnauthorizedAccessException
-from placeholder.utils.auth import JWTAuth
-from django.db import transaction
-from placeholder.schemas.base import ResultSchema
-
 
 meetup_router = Router(tags=["Meetup"])
 
@@ -31,22 +32,24 @@ def create_meetup(request, payload: MeetupCreateSchema, image: UploadedFile = Fi
 @meetup_router.get("", response={200: ResultSchema}, auth=JWTAuth(), by_alias=True)
 @handle_exceptions
 def get_meetups(request):
-    meetups = Meetup.objects.select_related('organizer').all()
+    meetups = Meetup.objects.select_related("organizer").all()
     response = []
     for meetup in meetups:
-        response.append(MeetupListResponseSchema(
-            id=meetup.id,
-            isOrganizer=meetup.organizer == request.auth,
-            organizer=OrganizerSchema(
-                nickname=meetup.organizer.nickname,
-                profile_image=meetup.organizer.image.url if meetup.organizer.image else None
-            ),
-            startedAt=meetup.started_at,
-            endedAt=meetup.ended_at,
-            adEndedAt=meetup.ad_ended_at,
-            isPublic=meetup.is_public,
-            image=meetup.image.url if meetup.image else "",
-        ))
+        response.append(
+            MeetupListResponseSchema(
+                id=meetup.id,
+                isOrganizer=meetup.organizer == request.auth,
+                organizer=OrganizerSchema(
+                    nickname=meetup.organizer.nickname,
+                    profile_image=meetup.organizer.image.url if meetup.organizer.image else None,
+                ),
+                startedAt=meetup.started_at,
+                endedAt=meetup.ended_at,
+                adEndedAt=meetup.ad_ended_at,
+                isPublic=meetup.is_public,
+                image=meetup.image.url if meetup.image else "",
+            )
+        )
     return 200, {"result": response}
 
 
@@ -76,12 +79,16 @@ def get_meetup(request, meetup_id: int):
 
 @meetup_router.put("/{meetup_id}", response={200: MeetupResponseSchema}, auth=JWTAuth(), by_alias=True)
 @handle_exceptions
-def update_meetup(request, meetup_id: int, payload: MeetupCreateSchema):
+def update_meetup(request, meetup_id: int, payload: MeetupCreateSchema, image: UploadedFile = File(None)):
     meetup = get_object_or_404(Meetup, id=meetup_id)
     if meetup.organizer != request.auth:
         raise UnauthorizedAccessException()
     for attr, value in payload.dict().items():
         setattr(meetup, attr, value)
+
+    if image:
+        meetup.image = image
+
     meetup.save()
     return meetup
 
