1a062a20fee3221f6aeed8ab2c1bea64c9db4743
lonnie <joonyeol.dev@gmail.com>
2025-01-09 09:50:55 +0900

    [PH-5] 알림 read api 추가

--- notification/apis/notification.py
+++ notification/apis/notification.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from django.db.models import F
 from ninja import Router
 
 from notification.models import Notification
 from notification.schemas.notification import NotificationListResultSchema
+from placeholder.schemas.base import ErrorSchema
 from placeholder.utils.auth import JWTAuth
 from placeholder.utils.decorators import handle_exceptions
 
@@ -15,3 +17,15 @@ def get_notifications(request):
     user = request.auth
     notifications = Notification.objects.filter(recipient=user)[:10]
     return 200, {"result": notifications}
+
+
+@notification_router.post(
+    "{notification_id}/read", response={204: None, 404: ErrorSchema}, auth=JWTAuth(), by_alias=True
+)
+@handle_exceptions
+def read_notification(request, notification_id):
+    user = request.auth
+    result = Notification.objects.filter(id=notification_id, recipient=user).update(is_public=~F("is_public"))
+    if not result:
+        return 404, {"message": "존재하지 않은 알림 입니다."}
+    return 204
--- notification/schemas/notification.py
+++ notification/schemas/notification.py
@@ -6,7 +6,7 @@ from ninja.orm import create_schema
 from notification.models.notification import Notification
 from placeholder.schemas.base import BaseSchema
 
-NotificationSchema = create_schema(Notification, fields=["message", "url", "is_read"])
+NotificationSchema = create_schema(Notification, fields=["id", "message", "url", "is_read"])
 
 
 class NotificationListResultSchema(BaseSchema):
