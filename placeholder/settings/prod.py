b6c4bdb4662ca4fed861fafdc374fba5e303950a
lonnie <joonyeol.dev@gmail.com>
2025-06-06 18:00:31 +0900

    [PH-22] 배포 준비

---
+++ placeholder/settings/prod.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from placeholder.settings.base import *  # noqa: F403, F401
+
+DEBUG = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+ALLOWED_HOSTS = [
+    "localhost",
+]
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # noqa: F405
+]
