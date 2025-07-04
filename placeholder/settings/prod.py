74e00f5ff770159f6964301564c94b4245b5a223
lonnie <joonyeol.dev@gmail.com>
2025-07-05 00:14:14 +0900

    [FEATURE] server settings

--- placeholder/settings/prod.py
+++ placeholder/settings/prod.py
@@ -4,15 +4,15 @@ from placeholder.settings.base import *  # noqa: F403, F401
 DEBUG = True
 
 CORS_ALLOWED_ORIGINS = [
-    "api.place-holder.site",
-    "www.place-holder.site",
-    "place-holder.site",
-]
-
-ALLOWED_HOSTS = [
     "https://api.place-holder.site",
     "https://www.place-holder.site",
     "https://place-holder.site",
+]
+
+ALLOWED_HOSTS = [
+    "api.place-holder.site",
+    "www.place-holder.site",
+    "place-holder.site",
 ]
 
 STATIC_URL = "static/"
